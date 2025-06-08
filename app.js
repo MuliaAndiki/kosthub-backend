8668251e03ef2c165fec8a2f58541ab211ade996
diki <muliaandiki@gmaiil.com>
2025-06-09 01:06:26 +0700

    feat: service endpoint for backend kosthub service

--- app.js
+++ app.js
@@ -23,13 +23,13 @@ const io = new SocketIOServer(server, {
 });
 global.io = io;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
+app.use("/", (req, res) => {
+  res.send("Kosthub BackEnd Service");
+});
 app.use("/uploads", express.static("uploads"));
-
-// Routes
 app.use("/api/kos", kosRoutes);
 app.use("/api/auth", authRouter);
 app.use("/api/reservase", reservaseRoutes);
@@ -40,7 +40,6 @@ const __dirname = dirname(__filename);
 app.use("/images", express.static(path.join(__dirname, "uploads/images")));
 
 io.on("connection", (socket) => {
-  
   socket.on("join", (userId) => {
     if (userId) socket.join(userId);
   });
@@ -53,13 +52,12 @@ const clientOptions = {
     deprecationErrors: true,
   },
 };
-// Connect MongoDB
+
 mongoose
   .connect(process.env.MONGO_URI, clientOptions)
   .then(() => {
     console.log("MongoDB Connected");
 
-    // Start server
     const PORT = process.env.PORT || 5000;
     server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
